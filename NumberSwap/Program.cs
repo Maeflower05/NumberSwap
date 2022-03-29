1fc01f648468912d74a2539890e2f8c177c951a7
Maeflower05 <annajacobsen1999@hotmail.com>
2022-03-29 08:37:10 +0200

    Add project files.

---
+++ NumberSwap/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Velkommen til Number Swap!");
+Console.Write("Indtast venligst dit første tal: ");
+string x = Console.ReadLine();
+Console.Write("Indtast venligst dit næste tal: ");
+string y = Console.ReadLine();
+
+//Der er TO måder at gøre det på. Den hurtige er dem her (linje 8-10.)
+//string v = x;
+//x = y;
+//y = v;
+
+//Men her har jeg bydt them om, og lavet dem til int istedet for string.
+Console.WriteLine($"\nDu har valgt dit første tal som {x} og næste som {y}.");
+int a = Convert.ToInt32(x);
+int b = Convert.ToInt32(y);
+(a, b) = (b, a);
+Console.WriteLine($"\nOg nu swapper vi! \nDit første tal er nu {a} og dit næste tal er {b}");
+Console.ReadLine();
